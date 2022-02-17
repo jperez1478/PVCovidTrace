cd8a59bcbe7fac03e84d73c30401a2f91490ae85
jperez1478 <jessicperez3@gmail.com>
2022-02-16 22:03:12 -0600

    Initial Commit

---
+++ pvCovidTrace/pvCovidTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  pvCovidTraceApp.swift
+//  pvCovidTrace
+//
+//  Created by Jessica Perez on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct pvCovidTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
